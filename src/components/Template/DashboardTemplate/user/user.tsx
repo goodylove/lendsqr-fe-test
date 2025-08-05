c239ae36f8a7554f3e6705c1bc3859019527c756
goodylove <goodyc474@gmail.com>
2025-08-05 11:28:38 +0100

    feat: implement search functionality on users page via query params

--- src/components/Template/DashboardTemplate/user/user.tsx
+++ src/components/Template/DashboardTemplate/user/user.tsx
@@ -1,42 +1,74 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Stats from "../../../common/Stats/stats";
 import "./user.modules.scss";
 import Table from "../../../common/Table/table";
 import type { User } from "../../../../types/types";
 import userData from "../../../../data/mock_users.json";
 import { useUserStatusStats } from "../../../../hooks/useUserStatusStats";
+import { useSearchParams } from "react-router-dom";
 
 function UserTemplate() {
   const data = useMemo(() => userData.users as User[], []);
-  const { active, pending, inactive } = useUserStatusStats(data);
+
+  const [searchParam] = useSearchParams();
+
+  const search = searchParam.get("search")?.toLowerCase() || "";
+  const [filteredUsers, setFilteredUsers] = useState<User[]>(data);
+  const { active, pending, inactive } = useUserStatusStats(filteredUsers);
+
+  useEffect(() => {
+    if (!search) {
+      setFilteredUsers(data);
+    } else {
+      const result = data.filter(
+        (user) =>
+          user.email.toLowerCase().includes(search) ||
+          user.username.toLowerCase().includes(search) ||
+          user.organization.toLowerCase().includes(search) ||
+          user.status.toLowerCase().includes(search) ||
+          user.id.toLowerCase().includes(search)
+      );
+      setFilteredUsers(result);
+    }
+  }, [search, data]);
 
   return (
     <div className="users-container">
       <h2>Users</h2>
-      <div className="users">
-        <Stats
-          icon="/svg/icons/all-users.svg"
-          title="users"
-          value={data.length}
-        />
-        <Stats
-          icon="/svg/icons/active-users.svg"
-          title="active users"
-          value={active}
-        />
-        <Stats
-          icon="/svg/icons/loan-users.svg"
-          title="Pending users "
-          value={pending}
-        />
-        <Stats
-          icon="/svg/icons/saving-users.svg"
-          title="Inactive users"
-          value={inactive}
-        />
-      </div>
 
-      <Table users={data} />
+      {filteredUsers.length === 0 ? (
+        <div className="empty-state">
+          <img src="/download.png" alt="No user selected" />
+          <h2>No Users Found</h2>
+        </div>
+      ) : (
+        <>
+          <div className="users">
+            <Stats
+              icon="/svg/icons/all-users.svg"
+              title="users"
+              value={data.length}
+            />
+            <Stats
+              icon="/svg/icons/active-users.svg"
+              title="active users"
+              value={active}
+            />
+            <Stats
+              icon="/svg/icons/loan-users.svg"
+              title="Pending users "
+              value={pending}
+            />
+            <Stats
+              icon="/svg/icons/saving-users.svg"
+              title="Inactive users"
+              value={inactive}
+            />
+          </div>
+
+          <Table users={filteredUsers} />
+        </>
+      )}
     </div>
   );
 }
--- src/components/common/SearchInput/searchInput.tsx
+++ src/components/common/SearchInput/searchInput.tsx
@@ -1,16 +1,31 @@
+import React from "react";
 import "./searchInput.modules.scss";
+import { useNavigate } from "react-router-dom";
 const SearchInput = () => {
+  const [searchTerm, setSearchTerm] = React.useState<string>("");
+
+  const navigate = useNavigate();
+
+  function handleSearch(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!searchTerm.trim()) return;
+    navigate(`/dashboard/users?search=${encodeURIComponent(searchTerm)}`);
+    setSearchTerm("");
+  }
+
   return (
-    <div className="search-container">
+    <form className="search-container" onSubmit={handleSearch}>
       <input
         type="text"
         className="search-input"
         placeholder="Search for anything"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <button className="search-button">
+      <button className="search-button" type="submit">
         <img src="/svg/search.svg" alt="search icon" />
       </button>
-    </div>
+    </form>
   );
 };
 
